adca107cbdf528e416c9181c96a41cb75a4939ea
Dave.Revell <Dave.Revell@caf00c8e-0b73-11de-8db4-35e3754be71a>
2009-03-29 08:20:59 +0000

    Added GPLv3 boilerplate

--- sign.sh
+++ sign.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# This is a simple script to save typing when it's time to sign a release
+# jar file. It assumes a number of things about the directory structure
+# and the location of SwiFTP.apk.
+
 jarsigner -keystore ../swiftp_private/swiftp.keystore ./SwiFTP.apk swiftp
 jarsigner -verify -verbose -certs SwiFTP.apk
