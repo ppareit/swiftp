80a2806668c750c8ac59f09bd89193ce12c902dc
Pieter Pareit <pieter.pareit@gmail.com>
2012-08-05 17:11:06 +0200

    Signing happens now from within eclipse

--- sign.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This is a simple script to save typing when it's time to sign a release
-# jar file. It assumes a number of things about the directory structure
-# and the location of SwiFTP.apk.
-
-jarsigner -keystore ../swiftp_private/swiftp.keystore ./SwiFTP.apk swiftp
-jarsigner -verify -verbose -certs SwiFTP.apk
