5d5f069ededfd01d776f3648fea2b9924c08ac48
Pieter Pareit <pieter.pareit@gmail.com>
2012-10-05 20:42:06 +0200

    Refactor and remove dependency on MyLog

--- src/be/ppareit/swiftp_free/server/CmdAbstractListing.java
+++ src/be/ppareit/swiftp_free/server/CmdAbstractListing.java
@@ -15,7 +15,7 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with SwiFTP.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 
 /*
  * Since the FTP verbs LIST and NLST do very similar things related to listing
@@ -28,67 +28,66 @@ package be.ppareit.swiftp_free.server;
 
 import java.io.File;
 
-import be.ppareit.swiftp_free.MyLog;
-
 import android.util.Log;
 
 public abstract class CmdAbstractListing extends FtpCmd {
-	protected static MyLog staticLog = new MyLog(CmdLIST.class.toString());
+    private static String TAG = "CmdAbstractListing"; // TODO: .class.getSimpleName() from
+                                                      // abstract class?
 
-	public CmdAbstractListing(SessionThread sessionThread, String input) {
-		super(sessionThread, CmdAbstractListing.class.toString());
-	}
+    public CmdAbstractListing(SessionThread sessionThread, String input) {
+        super(sessionThread, CmdAbstractListing.class.toString());
+    }
 
-	abstract String makeLsString(File file);
+    abstract String makeLsString(File file);
 
-	// Creates a directory listing by finding the contents of the directory,
-	// calling makeLsString on each file, and concatenating the results.
-	// Returns an error string if failure, returns null on success. May be
-	// called by CmdLIST or CmdNLST, since they each override makeLsString
-	// in a different way.
-	public String listDirectory(StringBuilder response, File dir) {
-		if(!dir.isDirectory()) {
-			return "500 Internal error, listDirectory on non-directory\r\n";
-		}
-		myLog.l(Log.DEBUG, "Listing directory: " + dir.toString());
+    // Creates a directory listing by finding the contents of the directory,
+    // calling makeLsString on each file, and concatenating the results.
+    // Returns an error string if failure, returns null on success. May be
+    // called by CmdLIST or CmdNLST, since they each override makeLsString
+    // in a different way.
+    public String listDirectory(StringBuilder response, File dir) {
+        if (!dir.isDirectory()) {
+            return "500 Internal error, listDirectory on non-directory\r\n";
+        }
+        Log.d(TAG, "Listing directory: " + dir.toString());
 
-		// Get a listing of all files and directories in the path
-		File[] entries = dir.listFiles();
-		if(entries == null) {
-			return "500 Couldn't list directory. Check config and mount status.\r\n";
-		}
-		myLog.l(Log.DEBUG, "Dir len " + entries.length);
-		for(File entry : entries) {
-			String curLine = makeLsString(entry);
-			if(curLine != null) {
-				response.append(curLine);
-			}
-		}
-		return null;
-	}
+        // Get a listing of all files and directories in the path
+        File[] entries = dir.listFiles();
+        if (entries == null) {
+            return "500 Couldn't list directory. Check config and mount status.\r\n";
+        }
+        Log.d(TAG, "Dir len " + entries.length);
+        for (File entry : entries) {
+            String curLine = makeLsString(entry);
+            if (curLine != null) {
+                response.append(curLine);
+            }
+        }
+        return null;
+    }
 
-	// Send the directory listing over the data socket. Used by CmdLIST and
-	// CmdNLST.
-	// Returns an error string on failure, or returns null if successful.
-	protected String sendListing(String listing) {
-		if(sessionThread.startUsingDataSocket()) {
-			myLog.l(Log.DEBUG, "LIST/NLST done making socket");
-		} else {
-			sessionThread.closeDataSocket();
-			return "425 Error opening data socket\r\n";
-		}
-		String mode = sessionThread.isBinaryMode() ? "BINARY" : "ASCII";
-		sessionThread.writeString(
-				"150 Opening "+mode+" mode data connection for file list\r\n");
-		myLog.l(Log.DEBUG, "Sent code 150, sending listing string now");
-		if(!sessionThread.sendViaDataSocket(listing)) {
-			myLog.l(Log.DEBUG, "sendViaDataSocket failure");
-			sessionThread.closeDataSocket();
-			return "426 Data socket or network error\r\n";
-		}
-		sessionThread.closeDataSocket();
-		myLog.l(Log.DEBUG, "Listing sendViaDataSocket success");
-		sessionThread.writeString("226 Data transmission OK\r\n");
-		return null;
-	}
+    // Send the directory listing over the data socket. Used by CmdLIST and
+    // CmdNLST.
+    // Returns an error string on failure, or returns null if successful.
+    protected String sendListing(String listing) {
+        if (sessionThread.startUsingDataSocket()) {
+            Log.d(TAG, "LIST/NLST done making socket");
+        } else {
+            sessionThread.closeDataSocket();
+            return "425 Error opening data socket\r\n";
+        }
+        String mode = sessionThread.isBinaryMode() ? "BINARY" : "ASCII";
+        sessionThread.writeString("150 Opening " + mode
+                + " mode data connection for file list\r\n");
+        Log.d(TAG, "Sent code 150, sending listing string now");
+        if (!sessionThread.sendViaDataSocket(listing)) {
+            Log.d(TAG, "sendViaDataSocket failure");
+            sessionThread.closeDataSocket();
+            return "426 Data socket or network error\r\n";
+        }
+        sessionThread.closeDataSocket();
+        Log.d(TAG, "Listing sendViaDataSocket success");
+        sessionThread.writeString("226 Data transmission OK\r\n");
+        return null;
+    }
 }
