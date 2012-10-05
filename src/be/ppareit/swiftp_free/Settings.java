3353f90b49ca593970e8f9aeaf54fc38d5489c0e
Pieter Pareit <pieter.pareit@gmail.com>
2012-10-05 18:06:15 +0200

    Refactor and remove dead code

--- src/be/ppareit/swiftp_free/Settings.java
+++ src/be/ppareit/swiftp_free/Settings.java
@@ -15,69 +15,56 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with SwiFTP.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 
 package be.ppareit.swiftp_free;
 
-import android.util.Log;
 
 public class Settings {
-	protected static int inputBufferSize = 256;
-	protected static boolean allowOverwrite = false;
-	protected static int dataChunkSize = 8192;  // do file I/O in 8k chunks 
-	protected static int sessionMonitorScrollBack = 10;
-	protected static int serverLogScrollBack = 10;
-	protected static int uiLogLevel = Log.INFO;
-	
-	public static int getUiLogLevel() {
-		return uiLogLevel;
-	}
+    protected static int inputBufferSize = 256;
+    protected static boolean allowOverwrite = false;
+    protected static int dataChunkSize = 8192; // do file I/O in 8k chunks
+    protected static int sessionMonitorScrollBack = 10;
+    protected static int serverLogScrollBack = 10;
 
-	public static void setUiLogLevel(int uiLogLevel) {
-		Settings.uiLogLevel = uiLogLevel;
-	}
+    public static int getInputBufferSize() {
+        return inputBufferSize;
+    }
 
-	public static int getInputBufferSize() {
-		return inputBufferSize;
-	}
+    public static void setInputBufferSize(int inputBufferSize) {
+        Settings.inputBufferSize = inputBufferSize;
+    }
 
-	public static void setInputBufferSize(int inputBufferSize) {
-		Settings.inputBufferSize = inputBufferSize;
-	}
+    public static boolean isAllowOverwrite() {
+        return allowOverwrite;
+    }
 
-	public static boolean isAllowOverwrite() {
-		return allowOverwrite;
-	}
+    public static void setAllowOverwrite(boolean allowOverwrite) {
+        Settings.allowOverwrite = allowOverwrite;
+    }
 
-	public static void setAllowOverwrite(boolean allowOverwrite) {
-		Settings.allowOverwrite = allowOverwrite;
-	}
+    public static int getDataChunkSize() {
+        return dataChunkSize;
+    }
 
-	public static int getDataChunkSize() {
-		return dataChunkSize;
-	}
+    public static void setDataChunkSize(int dataChunkSize) {
+        Settings.dataChunkSize = dataChunkSize;
+    }
 
-	public static void setDataChunkSize(int dataChunkSize) {
-		Settings.dataChunkSize = dataChunkSize;
-	}
+    public static int getSessionMonitorScrollBack() {
+        return sessionMonitorScrollBack;
+    }
 
-	public static int getSessionMonitorScrollBack() {
-		return sessionMonitorScrollBack;
-	}
+    public static void setSessionMonitorScrollBack(int sessionMonitorScrollBack) {
+        Settings.sessionMonitorScrollBack = sessionMonitorScrollBack;
+    }
 
-	public static void setSessionMonitorScrollBack(
-			int sessionMonitorScrollBack) 
-	{
-		Settings.sessionMonitorScrollBack = sessionMonitorScrollBack;
-	}
+    public static int getServerLogScrollBack() {
+        return serverLogScrollBack;
+    }
 
-	public static int getServerLogScrollBack() {
-		return serverLogScrollBack;
-	}
+    public static void setLogScrollBack(int serverLogScrollBack) {
+        Settings.serverLogScrollBack = serverLogScrollBack;
+    }
 
-	public static void setLogScrollBack(int serverLogScrollBack) {
-		Settings.serverLogScrollBack = serverLogScrollBack;
-	}
-	
-	
 }
