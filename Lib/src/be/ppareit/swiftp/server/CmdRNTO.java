daa9ec5966a55fdc196cc58e5553dfbdf76cf75d
Pieter Pareit <pieter.pareit@gmail.com>
2013-07-24 16:31:43 +0200

    Fix bug when renaming file (only case changes)

--- Lib/src/be/ppareit/swiftp/server/CmdRNTO.java
+++ Lib/src/be/ppareit/swiftp/server/CmdRNTO.java
@@ -20,6 +20,7 @@ along with SwiFTP.  If not, see <http://www.gnu.org/licenses/>.
 package be.ppareit.swiftp.server;
 
 import java.io.File;
+import java.io.IOException;
 
 import android.util.Log;
 
@@ -35,14 +36,14 @@ public class CmdRNTO extends FtpCmd implements Runnable {
 
     @Override
     public void run() {
-        Log.d(TAG, "RNTO executing\r\n");
+        Log.d(TAG, "RNTO executing");
         String param = getParameter(input);
         String errString = null;
         File toFile = null;
         mainblock: {
             Log.i(TAG, "param: " + param);
             toFile = inputPathToChrootedFile(sessionThread.getWorkingDir(), param);
-            Log.i(TAG, "RNTO parsed: " + toFile.getPath());
+            Log.i(TAG, "RNTO to file: " + toFile.getPath());
             if (violatesChroot(toFile)) {
                 errString = "550 Invalid name or chroot violation\r\n";
                 break mainblock;
@@ -52,7 +53,30 @@ public class CmdRNTO extends FtpCmd implements Runnable {
                 errString = "550 Rename error, maybe RNFR not sent\r\n";
                 break mainblock;
             }
-            if (!fromFile.renameTo(toFile)) {
+            Log.i(TAG, "RNTO from file: " + fromFile.getPath());
+            // TODO: this code is working around a bug that java6 and before cannot
+            // reliable move a file, once java7 is supported by Dalvik, this code can
+            // be replaced with Files.move()
+            File tmpFile = null;
+            try {
+                tmpFile = File.createTempFile(fromFile.getName(), null,
+                        sessionThread.getWorkingDir());
+                if (fromFile.isDirectory()) {
+                    String tmpFilePath = tmpFile.getPath();
+                    tmpFile.delete();
+                    tmpFile = new File(tmpFilePath);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                errString = "550 Error during rename operation\r\n";
+                break mainblock;
+            }
+            if (!fromFile.renameTo(tmpFile)) {
+                errString = "550 Error during rename operation\r\n";
+                break mainblock;
+            }
+            fromFile.delete();
+            if (!tmpFile.renameTo(toFile)) {
                 errString = "550 Error during rename operation\r\n";
                 break mainblock;
             }
