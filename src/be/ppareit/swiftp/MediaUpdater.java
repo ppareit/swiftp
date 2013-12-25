e507624dffa68928a1cc2ee02c6ce837381a6432
Pieter Pareit <pieter.pareit@gmail.com>
2013-12-25 18:52:29 +0100

    Fix FC when deleting file, issue #30

--- src/be/ppareit/swiftp/MediaUpdater.java
+++ src/be/ppareit/swiftp/MediaUpdater.java
@@ -7,6 +7,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
 import android.os.Environment;
 import android.util.Log;
 
@@ -20,45 +22,61 @@ import android.util.Log;
  */
 public enum MediaUpdater {
     INSTANCE;
-    
+
     private final static String TAG = MediaUpdater.class.getSimpleName();
 
     // the systembroadcast to remount the media is only done after a little while (5s)
     private static Timer sTimer = new Timer();
 
+    private static class ScanCompletedListener implements
+            MediaScannerConnection.OnScanCompletedListener {
+        @Override
+        public void onScanCompleted(String path, Uri uri) {
+            Log.i(TAG, "Scan completed: " + path + " : " + uri);
+        }
+
+    }
+
     public static void notifyFileCreated(String path) {
         if (Defaults.do_mediascanner_notify) {
             Log.d(TAG, "Notifying others about new file: " + path);
             Context context = FtpServerApp.getAppContext();
-            MediaScannerConnection.scanFile(context, new String[]{path}, null,
-                    new MediaScannerConnection.OnScanCompletedListener() {
-                        @Override
-                        public void onScanCompleted(String path, Uri uri) {
-                        }
-                    });
+            MediaScannerConnection.scanFile(context, new String[] { path }, null,
+                    new ScanCompletedListener());
         }
     }
 
     public static void notifyFileDeleted(String path) {
-        // The media mounted broadcast is very taxing on the system, so we only do this
-        // if for 5 seconds there was no same request, otherwise we wait again.
         if (Defaults.do_mediascanner_notify) {
             Log.d(TAG, "Notifying others about deleted file: " + path);
-            // the systembroadcast might have been requested already, cancel if so
-            sTimer.cancel();
-            // that timer is of no value any more, create a new one
-            sTimer = new Timer();
-            // and in 5s let it send the broadcast, might never hapen if before
-            // that time it gets canceled by this code path
-            sTimer.schedule(new TimerTask() {
-                @Override
-                public void run() {
-                    Log.d(TAG, "Sending ACTION_MEDIA_MOUNTED broadcast");
-                    final Context context = FtpServerApp.getAppContext();
-                    context.sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED, Uri
-                            .parse("file://" + Environment.getExternalStorageDirectory())));
-                }
-            }, 5000);
+            if (VERSION.SDK_INT < VERSION_CODES.KITKAT) {
+                // on older devices, fake a remount of the media
+                // The media mounted broadcast is very taxing on the system, so
+                // we only do this if for 5 seconds there was no same request,
+                // otherwise we wait again.
+                // the broadcast might have been requested already, cancel if so
+                sTimer.cancel();
+                // that timer is of no value any more, create a new one
+                sTimer = new Timer();
+                // and in 5s let it send the broadcast, might never hapen if
+                // before
+                // that time it gets canceled by this code path
+                sTimer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        Log.d(TAG, "Sending ACTION_MEDIA_MOUNTED broadcast");
+                        final Context context = FtpServerApp.getAppContext();
+                        context.sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED, Uri
+                                .parse("file://"
+                                        + Environment.getExternalStorageDirectory())));
+                    }
+                }, 5000);
+            } else {
+                // on newer devices, we hope that this works correctly:
+                Context context = FtpServerApp.getAppContext();
+                MediaScannerConnection.scanFile(context, new String[] { path }, null,
+                        new ScanCompletedListener());
+            }
         }
     }
 
