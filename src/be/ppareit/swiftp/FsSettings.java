9d0a3da2cde39a3745b73023eefc06dcdc96bd1a
Pieter Pareit <pieter.pareit@gmail.com>
2014-11-10 11:53:36 +0100

    Fix spacing

--- src/be/ppareit/swiftp/FsSettings.java
+++ src/be/ppareit/swiftp/FsSettings.java
@@ -62,8 +62,8 @@ public class FsSettings {
 
     public static int getPortNumber() {
         final SharedPreferences sp = getSharedPreferences();
-		// TODO: port is always an number, so store this accordenly
-		String portString = sp.getString("portNum", "2121");
+        // TODO: port is always an number, so store this accordenly
+        String portString = sp.getString("portNum", "2121");
         int port = Integer.valueOf(portString);
         Log.v(TAG, "Using port: " + port);
         return port;
