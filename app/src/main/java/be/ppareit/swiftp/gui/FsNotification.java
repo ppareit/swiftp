1620b2eda1e2248768ef8707fe5833df132b558b
Pieter Pareit <pieter.pareit@gmail.com>
2015-11-05 11:25:19 +0100

    Add settings action to notification

--- app/src/main/java/be/ppareit/swiftp/gui/FsNotification.java
+++ app/src/main/java/be/ppareit/swiftp/gui/FsNotification.java
@@ -19,41 +19,44 @@ along with SwiFTP.  If not, see <http://www.gnu.org/licenses/>.
 
 package be.ppareit.swiftp.gui;
 
-import java.net.InetAddress;
 import android.annotation.SuppressLint;
-import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
-import android.util.Log;
+
+import net.vrallev.android.cat.Cat;
+
+import java.net.InetAddress;
+
 import be.ppareit.swiftp.FsService;
 import be.ppareit.swiftp.FsSettings;
 import be.ppareit.swiftp.R;
 
 public class FsNotification extends BroadcastReceiver {
-    private static final String TAG = FsNotification.class.getSimpleName();
 
     private final int NOTIFICATIONID = 7890;
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d(TAG, "onReceive broadcast: " + intent.getAction());
-        if (intent.getAction().equals(FsService.ACTION_STARTED)) {
-            setupNotification(context);
-        } else if (intent.getAction().equals(FsService.ACTION_STOPPED)) {
-            clearNotification(context);
+        Cat.d("onReceive broadcast: " + intent.getAction());
+        switch (intent.getAction()) {
+            case FsService.ACTION_STARTED:
+                setupNotification(context);
+                break;
+            case FsService.ACTION_STOPPED:
+                clearNotification(context);
+                break;
         }
     }
 
     @SuppressLint("NewApi")
     private void setupNotification(Context context) {
-        Log.d(TAG, "Setting up the notification");
+        Cat.d("Setting up the notification");
         // Get NotificationManager reference
         String ns = Context.NOTIFICATION_SERVICE;
         NotificationManager nm = (NotificationManager) context.getSystemService(ns);
@@ -61,28 +64,24 @@ public class FsNotification extends BroadcastReceiver {
         // get ip address
         InetAddress address = FsService.getLocalInetAddress();
         if (address == null) {
-            Log.w(TAG, "Unable to retreive the local ip address");
+            Cat.w("Unable to retrieve the local ip address");
             return;
         }
         String iptext = "ftp://" + address.getHostAddress() + ":"
                 + FsSettings.getPortNumber() + "/";
 
         // Instantiate a Notification
-        int icon = R.drawable.notification;
-        CharSequence tickerText = String.format(
-                context.getString(R.string.notif_server_starting), iptext);
+        int icon = R.mipmap.notification;
+        CharSequence tickerText = String.format(context.getString(R.string.notif_server_starting), iptext);
         long when = System.currentTimeMillis();
 
         // Define Notification's message and Intent
         CharSequence contentTitle = context.getString(R.string.notif_title);
-        CharSequence contentText = String.format(context.getString(R.string.notif_text),
-                iptext);
+        CharSequence contentText = String.format(context.getString(R.string.notif_text), iptext);
 
         Intent notificationIntent = new Intent(context, FsPreferenceActivity.class);
-        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
-                | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
-                notificationIntent, 0);
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
 
         int stopIcon = android.R.drawable.ic_menu_close_clear_cancel;
         CharSequence stopText = context.getString(R.string.notif_stop_text);
@@ -90,18 +89,33 @@ public class FsNotification extends BroadcastReceiver {
         PendingIntent stopPendingIntent = PendingIntent.getBroadcast(context, 0,
                 stopIntent, PendingIntent.FLAG_ONE_SHOT);
 
-        Notification.Builder nb = new Notification.Builder(context) //
-                .setContentTitle(contentTitle) //
-                .setContentText(contentText) //
-                .setContentIntent(contentIntent) //
-                .setSmallIcon(icon) //
-                .setTicker(tickerText) //
-                .setWhen(when) //
+        int preferenceIcon = android.R.drawable.ic_menu_preferences;
+        CharSequence preferenceText = context.getString(R.string.notif_settings_text);
+        Intent preferenceIntent = new Intent(context, FsPreferenceActivity.class);
+        preferenceIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent preferencePendingIntent = PendingIntent.getActivity(context, 0, preferenceIntent, 0);
+
+        Notification.Builder nb = new Notification.Builder(context)
+                .setContentTitle(contentTitle)
+                .setContentText(contentText)
+                .setContentIntent(contentIntent)
+                .setSmallIcon(icon)
+                .setTicker(tickerText)
+                .setWhen(when)
                 .setOngoing(true);
 
         Notification notification = null;
+
+        // go from hight to low android version adding extra options
+        if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+            nb.setVisibility(Notification.VISIBILITY_PUBLIC);
+            nb.setCategory(Notification.CATEGORY_SERVICE);
+            nb.setPriority(Notification.PRIORITY_MAX);
+        }
         if (VERSION.SDK_INT >= VERSION_CODES.JELLY_BEAN) {
             nb.addAction(stopIcon, stopText, stopPendingIntent);
+            nb.addAction(preferenceIcon, preferenceText, preferencePendingIntent);
+            nb.setShowWhen(false);
             notification = nb.build();
         } else {
             notification = nb.getNotification();
@@ -110,15 +124,15 @@ public class FsNotification extends BroadcastReceiver {
         // Pass Notification to NotificationManager
         nm.notify(NOTIFICATIONID, notification);
 
-        Log.d(TAG, "Notication setup done");
+        Cat.d("Notification setup done");
     }
 
 
     private void clearNotification(Context context) {
-        Log.d(TAG, "Clearing the notifications");
+        Cat.d("Clearing the notifications");
         String ns = Context.NOTIFICATION_SERVICE;
         NotificationManager nm = (NotificationManager) context.getSystemService(ns);
         nm.cancelAll();
-        Log.d(TAG, "Cleared notification");
+        Cat.d("Cleared notification");
     }
 }
