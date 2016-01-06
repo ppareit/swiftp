2d7825cbe35d5cc84c1b63ebd0de5d5e98653a63
Pieter Pareit <pieter.pareit@gmail.com>
2016-01-06 20:16:36 +0100

    Run state changes from service

--- app/src/main/java/be/ppareit/swiftp/WifiStateChangeReceiver.java
+++ app/src/main/java/be/ppareit/swiftp/WifiStateChangeReceiver.java
@@ -19,9 +19,11 @@ along with SwiFTP.  If not, see <http://www.gnu.org/licenses/>.
 
 package be.ppareit.swiftp;
 
+import android.app.IntentService;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
@@ -40,11 +42,29 @@ public class WifiStateChangeReceiver extends BroadcastReceiver {
             return;
         }
         if (info.isConnected()) {
+            Cat.d("We are connecting to a wifi network");
+            Intent startServerIntent = new Intent(context, StartServerService.class);
+            context.startService(startServerIntent);
+        } else {
+            Cat.d("We are disconnected from wifi network");
+            Intent stopServerIntent = new Intent(context, StopServerService.class);
+            context.startService(stopServerIntent);
+        }
+    }
+
+    static public class StartServerService extends IntentService {
+
+        public StartServerService() {
+            super(StartServerService.class.getSimpleName());
+        }
+
+        @Override
+        protected void onHandleIntent(Intent intent) {
             if (FsService.isRunning()) {
                 Cat.v("We are connecting to a new wifi network on a running server, ignore");
                 return;
             }
-            WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+            WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
             WifiInfo wifiInfo = wifiManager.getConnectionInfo();
             if (wifiInfo == null) {
                 Cat.e("Null wifi info received, bailing");
@@ -54,15 +74,49 @@ public class WifiStateChangeReceiver extends BroadcastReceiver {
             if (FsSettings.getAutoConnectList().contains(wifiInfo.getSSID())) {
                 // sleep a short while so the network has time to truly connect
                 Util.sleepIgnoreInterupt(1000);
-                context.sendBroadcast(new Intent(FsService.ACTION_START_FTPSERVER));
+                sendBroadcast(new Intent(FsService.ACTION_START_FTPSERVER));
             }
-        } else {
-            Cat.d("We are disconnected from wifi network");
-            if (!FsService.isRunning()) {
-                Cat.v("Server is not running, ignore");
-                return;
-            }
-            context.sendBroadcast(new Intent(FsService.ACTION_STOP_FTPSERVER));
+        }
+    }
+
+    static public class StopServerService extends IntentService {
+
+        public StopServerService() {
+            super(StopServerService.class.getSimpleName());
+        }
+
+        @Override
+        protected void onHandleIntent(Intent intent) {
+            if (!FsService.isRunning()) return;
+
+            Util.sleepIgnoreInterupt(15000);
+            if (!FsService.isRunning()) return;
+
+            ConnectivityManager conManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+            NetworkInfo netInfo = conManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+            if (netInfo.isConnectedOrConnecting()) return;
+
+            Cat.d("Wifi connection disconnected and no longer connecting, stopping the ftp server");
+            sendBroadcast(new Intent(FsService.ACTION_STOP_FTPSERVER));
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
