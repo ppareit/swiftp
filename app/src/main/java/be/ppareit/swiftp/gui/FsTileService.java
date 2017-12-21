e551f0459c534f4230bace20754bbac57940e94b
Ashutosh Gangwar <ashutoshgngwr@gmail.com>
2017-12-21 22:36:58 +0530

    Add quick settings tile

---
+++ app/src/main/java/be/ppareit/swiftp/gui/FsTileService.java
@@ -0,0 +1,71 @@
+package be.ppareit.swiftp.gui;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import android.support.annotation.RequiresApi;
+
+import net.vrallev.android.cat.Cat;
+
+import java.net.InetAddress;
+
+import be.ppareit.swiftp.FsService;
+import be.ppareit.swiftp.FsSettings;
+import be.ppareit.swiftp.R;
+
+@RequiresApi(api = Build.VERSION_CODES.N)
+public class FsTileService extends TileService {
+
+    @Override
+    public void onClick() {
+        if(getQsTile().getState() == Tile.STATE_INACTIVE)
+            sendBroadcast(new Intent(FsService.ACTION_START_FTPSERVER));
+        else if(getQsTile().getState() == Tile.STATE_ACTIVE)
+            sendBroadcast(new Intent(FsService.ACTION_STOP_FTPSERVER));
+    }
+
+    @Override
+    public void onStartListening() {
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(FsService.ACTION_STARTED);
+        intentFilter.addAction(FsService.ACTION_STOPPED);
+        intentFilter.addAction(FsService.ACTION_FAILEDTOSTART);
+        registerReceiver(mFsActionsReceiver, intentFilter);
+        updateTileState();
+    }
+
+    @Override
+    public void onStopListening() {
+        unregisterReceiver(mFsActionsReceiver);
+    }
+
+    private void updateTileState() {
+        Tile tile = getQsTile();
+        if (FsService.isRunning()) {
+            tile.setState(Tile.STATE_ACTIVE);
+            // Fill in the FTP server address
+            InetAddress address = FsService.getLocalInetAddress();
+            if (address == null) {
+                Cat.v("Unable to retrieve wifi ip address");
+                tile.setLabel(getString(R.string.swiftp_name));
+                return;
+            }
+            tile.setLabel(address.getHostAddress() + ":" + FsSettings.getPortNumber());
+        } else {
+            tile.setState(Tile.STATE_INACTIVE);
+            tile.setLabel(getString(R.string.swiftp_name));
+        }
+        tile.updateTile();
+    }
+
+    BroadcastReceiver mFsActionsReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            updateTileState();
+        }
+    };
+}
