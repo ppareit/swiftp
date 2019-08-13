3dbe41c1f2c3a2b336389d4943174f94bf40ea8c
Pieter Pareit <pieter.pareit@gmail.com>
2019-08-13 14:31:33 +0200

    Add REST feature

--- app/src/main/java/be/ppareit/swiftp/server/CmdFEAT.java
+++ app/src/main/java/be/ppareit/swiftp/server/CmdFEAT.java
@@ -42,6 +42,7 @@ public class CmdFEAT extends FtpCmd implements Runnable {
         // All hashing algorithms supported by all Android versions
         // See https://developer.android.com/reference/java/security/MessageDigest.html
         sessionThread.writeString(" HASH MD5;SHA-1;SHA-256;SHA-384;SHA-512\r\n");
+        sessionThread.writeString(" REST STREAM\r\n");
         sessionThread.writeString(" RANG STREAM\r\n");
         sessionThread.writeString("211 End\r\n");
         Log.d(TAG, "run: Gave FEAT");
