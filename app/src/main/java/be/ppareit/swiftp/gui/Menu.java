16b17d5f2d43c120a465cb29a47e911a8a2c03a7
Pieter Pareit <pieter.pareit@gmail.com>
2026-08-18 20:19:16 +0200

    Add missing GPL licenses to source files

--- app/src/main/java/be/ppareit/swiftp/gui/Menu.java
+++ app/src/main/java/be/ppareit/swiftp/gui/Menu.java
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 package be.ppareit.swiftp.gui;
 
 import android.content.ActivityNotFoundException;
--- app/src/main/java/be/ppareit/swiftp/utils/StorageProbe.kt
+++ app/src/main/java/be/ppareit/swiftp/utils/StorageProbe.kt
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 package be.ppareit.swiftp.utils
 
 import android.os.Environment
